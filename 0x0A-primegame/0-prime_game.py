058069e33019a5ffcf60bbbe9edaf285381b65ff
Eric20Junior <eric20junior@gmail.com>
2022-08-10 18:17:33 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""prime game"""
+""" Prime Game  """
+
 
 def isWinner(x, nums):
-    """Return the winner of the prime game"""
+    """ Returns the winner of the Prime Game """
     if x == 0 or x == -1:
         return None
     elif x == 10000:
