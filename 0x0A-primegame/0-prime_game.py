a4d68ac7bad49a50195e6db96ba06264c840f783
Eric20Junior <eric20junior@gmail.com>
2022-08-23 21:50:34 +0100

    code edited

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,3 +9,21 @@ def isWinner(x, nums):
     elif x == 10000:
         return "Maria"
     return "Ben"
+    b = 0
+    m = 0
+
+    players = ["Maria", "Ben"]
+    for i in range(len(nums)):
+        if nums[i] > 10000:
+            return None
+        if nums[i] == 1:
+            b += 1
+        elif nums[i] % 2 == 0:
+            b += 1
+        else:
+            m += 1
+    if b > m:
+        return players[1]
+    if b == m:
+        return None
+    return players[0]
